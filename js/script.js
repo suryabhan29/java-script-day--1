98a0bb6fba2e11b31d23a034bf117e2b7bb59708
suryabhan29 <suryabhansingh1029@gmail.com>
2022-12-09 15:51:23 +0530

    commit 1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+console.log('Hello form external file i am a external file and code is cheak to top to bottom and my linking is done to upr by head and body tag so my name is top'); // this is statement 1
+
+var x = 50;
+var y = 30;
+console.log(x + y); // statement 2
+console.log(x*y); // statement 3
